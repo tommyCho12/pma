5efd76aed226b0e181e63baa25d325c2da24085b
Tomas Chovanec <tomas.chovanec@siemens-healthineers.com>
2025-12-01 16:44:25 +0100

    LocalServicePort import update

--- src/test/java/com/pma/controllers/HttpRequestTest.java
+++ src/test/java/com/pma/controllers/HttpRequestTest.java
@@ -5,15 +5,14 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertTrue;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @RunWith(SpringRunner.class)
-public class HttpRequestTest
-{
+public class HttpRequestTest {
     @LocalServerPort
     private int port;
 
@@ -21,9 +20,9 @@ public class HttpRequestTest
     private TestRestTemplate restTemplate;
 
     @Test
-    public void isVersionNumberDisplayedOnHomePage_thenSuccess(){
+    public void isVersionNumberDisplayedOnHomePage_thenSuccess() {
         String renderedHtml = this.restTemplate.getForObject("http://localhost:" + port + "/", String.class);
-//        System.out.println("\n\n\n\n\n\n" + renderedHtml + "\n\n\n\n\n\n");
+        // System.out.println("\n\n\n\n\n\n" + renderedHtml + "\n\n\n\n\n\n");
         assertTrue(renderedHtml.contains("0.0.1"));
     }
 }
